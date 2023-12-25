a9a650a98acd083bfe00a27f4e48336ede86fd72
tync0 <isgenderlituncay2004@gmail.com>
2023-12-25 13:41:41 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -3,12 +3,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-int main(int argc, char *argv[])
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
 {
-	char *buf = malloc(100);
 	size_t x = 100;
-	char *av[2];
-	int ex;
+	char *buf = malloc(x), *cmd;
+	char *av[10] = {NULL};
+	int ex, i = 0;
 
 	if (!buf)
 		exit(98);
@@ -16,13 +20,16 @@ int main(int argc, char *argv[])
 		printf("#cisfun$ ");
 		getline(&buf, &x, stdin);
 		buf[strcspn(buf, "\n")] = '\0';
-		av[0] = strdup(buf);
-		av[1] = NULL;
+		cmd = strtok(buf, " ");
+		while (cmd)
+		{
+			av[i++] = strdup(cmd);
+			cmd = strtok(NULL, " ");
+		}
 		ex = execvp(av[0], av);
 		if (ex == -1)
 		{
-			sprintf(buf, "%s: ", argv[0]);
-			perror(buf);
+			perror("Error: ");
 		}
 	} while (1);
 	return (0);

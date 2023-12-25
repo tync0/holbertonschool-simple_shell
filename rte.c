69c3aff12e3b7a5040f0cb62e2eb2fb8d011c1a6
tync0 <isgenderlituncay2004@gmail.com>
2023-12-25 02:44:32 -0800

    rte.c

--- rte.c
+++ rte.c
@@ -2,21 +2,28 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-int main(void)
+#include <sys/wait.h>
+int main(int argc, char *argv[])
 {
 	char *buf = malloc(100);
 	size_t x = 100;
 	char *av[2];
+	int ex;
 
 	if (!buf)
 		exit(98);
-	getline(&buf, &x, stdin);
-	buf[strcspn(buf, "\n")] = '\0';
-	av[0] = strdup(buf);
-	av[1] = NULL;
-	if (execvp(av[0], av) == -1)
-	{
-		perror("Error: ");
-	}
+	do {
+		printf("#cisfun$ ");
+		getline(&buf, &x, stdin);
+		buf[strcspn(buf, "\n")] = '\0';
+		av[0] = strdup(buf);
+		av[1] = NULL;
+		ex = execvp(av[0], av);
+		if (ex == -1)
+		{
+			sprintf(buf, "%s: ", argv[0]);
+			perror(buf);
+		}
+	} while (1);
 	return (0);
 }

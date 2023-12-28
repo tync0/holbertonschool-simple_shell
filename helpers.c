7c3faafa687516472168c33eaa702c5fba65d46c
tync0 <isgenderlituncay2004@gmail.com>
2023-12-28 04:59:24 -0800

    0.3

--- helpers.c
+++ helpers.c
@@ -5,56 +5,54 @@
  * @command: string to split
  * Return: returns an array of strings
  */
-char **split(char *command)
-{
-char *token, **tokens;
-int i = 0;
+char **split(char *command) {
+	char *token, **tokens;
+	int i = 0;
 
-tokens = malloc(sizeof(char *) * SIZE);
-if (!tokens)
-{
-perror("Malloc Error: ");
-exit(98);
+	tokens = malloc(sizeof(char *) * SIZE);
+	if (!tokens)
+	{
+		perror("Malloc Error: ");
+		exit(98);
+	}
+	token = strtok(command, " \t\n");
+	while (token)
+	{
+		tokens[i++] = strdup(token);
+		token = strtok(NULL, " \t\n");
+	}
+	tokens[i] = NULL;
+	return tokens;
 }
 
-token = strtok(command, " \t\n");
-while (token)
-{
-tokens[i++] = strdup(token);
-token = strtok(NULL, " \t\n");
-}
-tokens[i] = NULL;
-return (tokens);
-}
 /**
- * pre_execute - executes a command
+ * pre_execute - prepares and executes a command
  * @command: command to execute
- * @tmp: tmp variable
+ * @tmp: temporary variable for memory management
  * Return: void
-*/
-void pre_execute(char *command, char *tmp)
-{
-pid_t pid;
-int status;
-pid = fork();
-if (pid == -1)
-{
-free(command);
-perror("Error");
-}
-else if (pid == 0)
-{
-execute(command);
-free(tmp);
-exit(0);
-}
-else
-{
-waitpid(pid, &status, 0);
-if (WIFEXITED(status))
-status = WEXITSTATUS(status);
-free(tmp);
-}
+ */
+void pre_execute(char *command, char *tmp) {
+	pid_t pid;
+	int status;
+	pid = fork();
+	if (pid == -1)
+	{
+		free(command);
+		perror("Error");
+	} 
+	else if (pid == 0)
+	{
+		execute(command);
+		free(tmp);
+		exit(0);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			status = WEXITSTATUS(status);
+		free(tmp);
+	}
 }
 
 /**
@@ -62,28 +60,23 @@ free(tmp);
  * @command: command to execute
  * Return: returns 0 on success
  */
-int execute(char *command)
-{
-char **arr;
-
-arr = split(command);
-if (arr == NULL)
-{
-perror("Error");
-exit(1);
-}
-
-if (execvp(command, arr) == -1)
-{
-perror("Error execve");
-free_arr(arr);
-free(command);
-exit(1);
-}
-
-free_arr(arr);
-free(command);
-return (0);
+int execute(char *command) {
+	char **arr = split(command);
+	if (arr == NULL)
+	{
+		perror("Error");
+		exit(1);
+	}
+	if (execvp(arr[0], arr) == -1)
+	{
+		perror("Error execve");
+		free_arr(arr);
+		free(command);
+		exit(1);
+	}
+	free_arr(arr);
+	free(command);
+	return (0);
 }
 
 /**
@@ -91,15 +84,10 @@ return (0);
  * @arr: array to free
  * Return: void
  */
-void free_arr(char **arr)
-{
-int i = 0;
-
-while (arr[i])
-{
-free(arr[i]);
-i++;
-}
-free(arr);
+void free_arr(char **arr) {
+	int i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
 }
 
--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * main - main function of the shell
@@ -6,43 +10,44 @@
  */
 int main(void)
 {
-char *command, *tmp = NULL;
-int read;
-size_t size;
+	char *command, *tmp = NULL;
+	int read;
+	size_t size;
 
-while (1)
-{
-command = NULL;
-if (isatty(STDIN_FILENO))
-printf("$ ");
-fflush(stdout);
-read = getline(&command, &size, stdin);
-if (read == -1)
-{
-free(command);
-exit(0);
-}
-command[read - 1] = '\0';
-tmp = command;
-if (command == NULL)
-{
-free(tmp);
-exit(0);
-}
-while (command[0] == ' ' || command[0] == '\t')
-command++;
+	while (1)
+	{
+		command = NULL;
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		fflush(stdout);
+		read = getline(&command, &size, stdin);
+		if (read == -1)
+		{
+			free(command);
+			exit(0);
+		}
+		command[read - 1] = '\0';
+		tmp = command;
+		if (command == NULL)
+		{
+			free(tmp);
+			exit(0);
+		}
+		while (command[0] == ' ' || command[0] == '\t')
+			command++;
 
-if (command[0] == '\n' || command[0] == '\0')
-{
-free(tmp);
-continue;
+		if (command[0] == '\n' || command[0] == '\0')
+		{
+			free(tmp);
+			continue;
+		}
+		if (strcmp(command, "exit") == 0)
+		{
+			free(tmp);
+			exit(0);
+		}
+		pre_execute(command, tmp);
+	}
+	return (0);
 }
-if (strcmp(command, "exit") == 0)
-{
-free(tmp);
-exit(0);
-}
-pre_execute(command, tmp);
-}
-return (0);
-}
+
